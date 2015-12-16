e00c19ec1413a11c350c79777d5813b72a9f3dd6
Eyeball Edward <edwardyeung.private@gmail.com>
2015-12-16 22:35:35 +0800

    help and kill, @frodberserk you do the rest.

---
+++ Chatbot-Unibot-Commands.js
@@ -0,0 +1,22 @@
+var killTypes = ['$1 was murdered',
+    'Voldermort (aka Shadow Wizard) used Avada Kedavra on $1', '$1 disappeared for no reason',
+    '$1 played too much Minecraft and got eaten by a zombie', '$1 sleeps with the fishes',
+    '$1 has been entered into a Death Note', '$1 was accidentally decapitated in an old factory',
+    'A noose appeared around $1\'s neck and he tripped and fell off a cliff', 'An axe fell on $1\'s head.',
+    'in\u0252z\u0258m\u0252\u042f.A.M poured trifluoromethanesulfonic acid on $1'];
+
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports = {
+    help: function (args, room) {
+        room.sendMessage('Hi. I\'m Uni\\*\'s Chatbot, in TFHCB, to challenge annoying girls who think they can fight programming.');
+    }, kill: function (args, room) {
+        if (args.length === 0) {
+            room.sendMessage('Kill who?');
+        } else {
+            room.sendMessage(killTypes[randNum(0, killTypes.length - 1)].replace(/\$1/g, args.join(' ')));
+        }
+    }
+};
--- Chatbot-Unibot.js
+++ Chatbot-Unibot.js
@@ -1,5 +1,6 @@
 var ChatAPI = require('./modules/JS-TFHCAPI');
 var mongo = require('mongodb').MongoClient;
+var commands = require('./Chatbot-Unibot-Commands');
 
 var unibot;
 mongo.connect('mongodb://localhost:27017/TFHWebSite', {}, function (err, db) {
@@ -10,13 +11,23 @@ mongo.connect('mongodb://localhost:27017/TFHWebSite', {}, function (err, db) {
 });
 module.exports = function (id) {
     new ChatAPI('UniBot', unibot.realPass, 'localhost:3000', function (client) {
-        //console.log(client);
         client.getRoom(id, function (err, room) {
             room.join();
             room.watch(function (event) {
                 if (event.getType() === 1) {
-                    if (event.getContent().startsWith('!!/')) {
-                        console.log(event.getContent());
+                    if (event.getContent().startsWith('!!/') && event.getContent().length > 3) {
+                        var parts = event.getContent().split(/!!\/([^ ]+) ?(.+)?/).filter(function (e) {
+                            return !!e;
+                        }).map(function (e) {
+                            return e.trim();
+                        });
+                        var command = parts[0];
+                        var args = parts.slice(1);
+                        if (typeof commands[command] !== 'undefined') {
+                            commands[command](args, room);
+                        } else {
+                            room.sendMessage('@' + event.getSender().replace(/[ \\|{}\[\];:'",<.>\/?!@#$%^&*\(\)_\-+=]/g, '') + ': Command ' + command + ' is not found.');
+                        }
                     }
                 }
             });
